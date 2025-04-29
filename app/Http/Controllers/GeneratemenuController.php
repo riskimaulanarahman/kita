5296cac4a0e658c12fcfec6273a8f5cc4445af59
Riski Maulana Rahman <61986465+riskimaulanarahman@users.noreply.github.com>
2025-04-29 11:36:18 +0800

    gm

--- app/Http/Controllers/GeneratemenuController.php
+++ app/Http/Controllers/GeneratemenuController.php
@@ -13,93 +13,99 @@ class GeneratemenuController extends Controller
 
     public function index($route)
     {
-        // return $route;
-        // $checksidemenu = SideMenu::where('route', $route)->count();
 
-        // if($checksidemenu > 0) {
-            $sidemenu = SideMenu::select(['id', 'title', 'icon_id', 'sequence_id', 'parent_id', 'is_active', 'is_admin'])
-            ->where('route', $route)
-            ->first();
-        // } else {
-        //     $sidemenu = SideMenu::select(['id', 'title', 'icon_id', 'sequence_id', 'parent_id', 'is_active', 'is_admin'])
-        //     ->where('route', 'api')
-        //     ->first(); //api
-        // }
+        $sidemenu = SideMenu::select(['id', 'title', 'icon_id', 'sequence_id', 'parent_id', 'is_active', 'is_admin', 'companyList'])
+        ->where('route', $route)
+        ->first();
 
-            $sequence = Sequence::select(['title', 'is_active'])
-            ->where('id', $sidemenu->sequence_id)
-            ->first();
-            $icon = Icon::select('name')->where('id', $sidemenu->icon_id)->first();
-            $menu = array();
-            // Check active menu or sequence
-            if ($sequence->is_active || $sidemenu->is_active) {
+        // jika page/route tidak ditemukan direct to 404 view
+        if(!$sidemenu) {
+            abort('404');
+        }
 
-                $menu = array(
-                    'icon' => $icon->name,
-                    'module' => $sequence->title,
-                    'title' => $sidemenu->title,
-                    'active' => $sidemenu->is_active,
-                    'menu' => array()
-                );
-                
-            }
-            // secondary menu queries
-            $secondary_menu = Sidemenu::select(['id', 'icon_id', 'title', 'route'])->whereRaw('parent_id like ?', [$sidemenu->id])->get();
-            // check secondary menu have value
-            if (count($secondary_menu) > 0) {
-                // Get secondary menu
-                foreach ($secondary_menu as $key => $item_menu) {
-                    $icon_menu = Icon::select('name')->where('id', $item_menu->icon_id)->first();
+        $sequence = Sequence::select(['title', 'is_active'])
+        ->where('id', $sidemenu->sequence_id)
+        ->first();
+        $icon = Icon::select('name')->where('id', $sidemenu->icon_id)->first();
+        $menu = array();
+        // Check active menu or sequence
+        if ($sequence->is_active || $sidemenu->is_active) {
 
-                    // push array in to menu
-                    array_push($menu['menu'], array(
-                        'icon' => $icon_menu->name,
-                        'title' => $item_menu->title,
-                        'route' => $item_menu->route,
-                        'submenu' => array(),
-                        'data' => null
-                    ));
+            $menu = array(
+                'icon' => $icon->name,
+                'module' => $sequence->title,
+                'title' => $sidemenu->title,
+                'active' => $sidemenu->is_active,
+                'menu' => array()
+            );
+            
+        }
+        // secondary menu queries
+        $secondary_menu = Sidemenu::select(['id', 'icon_id', 'title', 'route'])->whereRaw('parent_id like ?', [$sidemenu->id])->get();
+        // check secondary menu have value
+        if (count($secondary_menu) > 0) {
+            // Get secondary menu
+            foreach ($secondary_menu as $key => $item_menu) {
+                $icon_menu = Icon::select('name')->where('id', $item_menu->icon_id)->first();
 
-                    // submenus queries
-                    $submenu = Sidemenu::select(['id', 'icon_id', 'title', 'route', 'is_active'])->whereRaw('parent_id like ? and is_secondary_menu like 1', [$item_menu->id])->get();
-                    // check have submenu item
-                    if (count($submenu) > 0) {
-                        // get submenu
-                        foreach ($submenu as $item_submenu) {
-                            $icon_submenu = Icon::select('name')->where('id', $item_submenu->icon_id)->first();
-                            // push array into sub menu
-                            array_push(
-                                $menu['menu'][$key]['submenu'],
-                                array(
-                                    'icon' => $icon_submenu->name,
-                                    'title' => $item_submenu->title,
-                                    'route' => $item_submenu->route,
-                                    'active' => $item_submenu->is_active,
-                                    'data' => null
+                // push array in to menu
+                array_push($menu['menu'], array(
+                    'icon' => $icon_menu->name,
+                    'title' => $item_menu->title,
+                    'route' => $item_menu->route,
+                    'submenu' => array(),
+                    'data' => null
+                ));
 
-                                )
-                            );
-                        }
-                    } else {
-                        $menu['menu'][$key]['submenu'] = null;
+                // submenus queries
+                $submenu = Sidemenu::select(['id', 'icon_id', 'title', 'route', 'is_active'])->whereRaw('parent_id like ? and is_secondary_menu like 1', [$item_menu->id])->get();
+                // check have submenu item
+                if (count($submenu) > 0) {
+                    // get submenu
+                    foreach ($submenu as $item_submenu) {
+                        $icon_submenu = Icon::select('name')->where('id', $item_submenu->icon_id)->first();
+                        // push array into sub menu
+                        array_push(
+                            $menu['menu'][$key]['submenu'],
+                            array(
+                                'icon' => $icon_submenu->name,
+                                'title' => $item_submenu->title,
+                                'route' => $item_submenu->route,
+                                'active' => $item_submenu->is_active,
+                                'data' => null
+
+                            )
+                        );
                     }
+                } else {
+                    $menu['menu'][$key]['submenu'] = null;
                 }
-            };
-            // if($menu['title'] == 'api')
-            // return false;
-            // dd($menu);
-            // return view
-            $viewGrid = view('grid.index')->with('data', $menu);
-            if($menu['active'] == true) {
-                if(isset($this->getAuth()->isAdmin)) {
+            }
+        };
+        
+        // return view
+        $viewGrid = (($sidemenu->sequence_id !== 2) && ($sidemenu->sequence_id !== 3)) ? view('grid.index')->with('data', $menu) : redirect()->route($route); // kondisi jika section tidak sama dengan dashboard
+        if($menu != null) {
+            if(isset($this->getAuth()->isAdmin)) {
                     if($this->getAuth()->isAdmin == 1) {
                         return $viewGrid;
                     } else if ($this->getAuth()->isAdmin == 0) {
                         if($sidemenu->is_admin == 0){
-                            return $viewGrid;
+                            $empBU = $this->getEmployeeID()->companycode;
+                            if($sidemenu->companyList == null || $sidemenu->companyList == "") {
+                                return $viewGrid;
+                            } else {
+                                $buString = $sidemenu->companyList;
+                                $buArray = explode(",", $buString); // Mengubah string menjadi array berdasarkan pemisah koma
+                                if (!in_array($empBU, $buArray)) { // Memeriksa apakah $sidemenu->companyList tidak ada dalam array $buArray
+                                    return view('errors.401');
+                                } else {
+                                    return $viewGrid;
+                                }
+                            }
                         } else {
                             $checkaccess = Useraccess::join('side_menus','tbl_useraccess.module_id','side_menus.modules')
-                            ->where('user_id',$this->getAuth()->id)
+                            ->where('employee_id',$this->getAuth()->id) // employee_id disini maksudnya adalah user_id pada table users
                             ->where('allowView',true)
                             ->get();
                             if($checkaccess) {
@@ -111,18 +117,13 @@ class GeneratemenuController extends Controller
                             }
                         }
                         return view('errors.401');
-
                     } 
-                } else {
-                    return redirect('/');
-                }
             } else {
-                return view('errors.404');
+                return redirect('/');
             }
-
-
-        // }
-
+        } else {
+            return view('errors.404');
+        }
 
     }
 }
